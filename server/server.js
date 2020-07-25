9edf27315dfcffb5a4eef25e1c4f0df223ae887b
hobaek <smilgaru@gmail.com>
2020-07-25 11:52:39 -0700

    install session, flash, passport,passport-local

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const flash = require('express-flash');
 const PORT = 3000;
 
 /*
@@ -11,6 +14,14 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(
+	session({
+		secret: 'secret',
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+app.use(flash());
 
 /*
  * Start server
