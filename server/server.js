6230a41e81452e71c0277e931c67af18dba00d0c
hobaek <smilgaru@gmail.com>
2020-07-25 15:29:05 -0700

    add global handller

--- server/server.js
+++ server/server.js
@@ -35,10 +35,22 @@ app.post('/api/signup', async (req, res) => {
 		(err, result) => {
 			if (err) {
 				console.log(err);
+				return;
 			}
-			res.send('success');
+			res.redirect('/');
 		}
 	);
+});
+
+app.use((err, req, res, next) => {
+	const defaultErr = {
+		log: 'Express error handler caught unknown middleware error',
+		status: 400,
+		message: { err: 'An error occurred' },
+	};
+	const errorObj = Object.assign({}, defaultErr, err);
+	console.log(errorObj.log);
+	return res.status(errorObj.status).json(errorObj.message);
 });
 
 /*
