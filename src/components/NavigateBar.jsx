2cc23bdf29cf72e0da9af6a7c281ce06541b6b7c
haejinjo <h3jnjo@gmail.com>
2020-07-25 18:21:54 -0700

    Make NavigateBar responsive, fill horizontal, and fix buttons

--- src/components/NavigateBar.jsx
+++ src/components/NavigateBar.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import { Navbar, Nav, Form, FormControl, Button, Container} from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap';
 
 const NavigateBar = () => {
   return (
-    <Container>
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
-      </Navbar>
-    </Container>
+    <Navbar bg="primary" variant="dark">
+      {/* TODO: Point this href to `/explore` if User is authenticated */}
+      <Navbar.Brand href="/">Scratch Project</Navbar.Brand>
+      {/* Set class for Login and Signup button Nav item to `margin-left: auto;`*/}
+      <Nav className="ml-auto">
+        <Nav.Link href="login">Login</Nav.Link>
+        <Nav.Link href="signup">Signup</Nav.Link>
+      </Nav>
+    </Navbar>
   );
+
+  // Search Bar Component
+  // <Form inline>
+  // <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+  // <Button variant="outline-light">Search</Button>
+  // </Form>
 }
+
 
 export default NavigateBar;
