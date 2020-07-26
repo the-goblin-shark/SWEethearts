c1a59f27a765743087e17f88b8f4a0d3cef18a9c
hobaek <smilgaru@gmail.com>
2020-07-26 16:24:19 -0700

    change password to hashedpassword in authController

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -4,22 +4,22 @@ const model = require('../Models/model.js');
 const authController = {};
 
 authController.register = async (req, res, next) => {
-  const { username, password, email } = req.body;
-  const queryText = `INSERT INTO User_credentials (username,password,email) VALUES ($1,$2,$3)`;
-  const hashedPassWord = await bcrypt.hash(password, 10);
+	const { username, password, email } = req.body;
+	const queryText = `INSERT INTO User_credentials (username,password,email) VALUES ($1,$2,$3)`;
+	const hashedPassWord = await bcrypt.hash(password, 10);
 
-  model.query(queryText, [username, password, email], (err) => {
-    if (err) {
-      console.log(err);
-      return next({
-        log: `error occurred at register middleware. error message is: ${err}`,
-        status: 400,
-        message: { err: 'An error occurred' },
-      });
-    }
-    return next();
-    // res.redirect('/');
-  });
+	model.query(queryText, [username, hashedPassWord, email], (err) => {
+		if (err) {
+			console.log(err);
+			return next({
+				log: `error occurred at register middleware. error message is: ${err}`,
+				status: 400,
+				message: { err: 'An error occurred' },
+			});
+		}
+		return next();
+		// res.redirect('/');
+	});
 };
 
 module.exports = authController;
