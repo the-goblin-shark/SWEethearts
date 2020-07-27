7e7c844c90b5c4685b6dc3df92ea34a22d1c559c
hobaek <smilgaru@gmail.com>
2020-07-27 12:14:14 -0700

    add search function in search bar

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,130 +1,141 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import {
-  Container,
-  Col,
-  Row,
-  Form,
-  Card,
-  Button,
-  CardDeck,
+	Container,
+	Col,
+	Row,
+	Form,
+	Card,
+	Button,
+	CardDeck,
 } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import Spinner from './Spinner';
 
 const Explore = (props) => {
-  const [response, setResponse] = useState([
-    {
-      idea_id: '',
-      name: '',
-      description: '',
-      why: '',
-      when_start: '',
-      when_end: '',
-      who: '',
-      creator_username: '',
-      image: '',
-    },
-  ]);
+	const [response, setResponse] = useState([
+		{
+			idea_id: '',
+			name: '',
+			description: '',
+			why: '',
+			when_start: '',
+			when_end: '',
+			who: '',
+			creator_username: '',
+			image: '',
+		},
+	]);
+	const [query, setQuery] = useState('');
 
-  useEffect(() => {
-    fetch('api/explore', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((data) => data.json())
-      .then((data) => setResponse(data));
-  }, []);
+	useEffect(() => {
+		fetch(`api/explore`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((data) => data.json())
+			.then((data) => setResponse(data));
+	}, []);
 
-  // get technologies
-  const techStack = ['HTML', 'JavaScript', 'Python', 'NodeJS'];
-  // Generate checkbox component for each technology
-  const generateTech = techStack.map((tech, idx) => {
-    return (
-      <Form key={idx}>
-        <div key="checkbox" className="mb-5 mt-5 ml-5">
-          <Form.Check type="checkbox">
-            <Form.Check.Input type="checkbox" isValid />
-            <Form.Check.Label className="ml-2">
-              {' '}
-              <h2>{tech}</h2>{' '}
-            </Form.Check.Label>
-          </Form.Check>
-        </div>
-      </Form>
-    );
-  });
+	// get technologies
+	const techStack = ['HTML', 'JavaScript', 'Python', 'NodeJS'];
+	// Generate checkbox component for each technology
+	const generateTech = techStack.map((tech, idx) => {
+		return (
+			<Form key={idx}>
+				<div key="checkbox" className="mb-5 mt-5 ml-5">
+					<Form.Check type="checkbox">
+						<Form.Check.Input type="checkbox" isValid />
+						<Form.Check.Label className="ml-2">
+							{' '}
+							<h2>{tech}</h2>{' '}
+						</Form.Check.Label>
+					</Form.Check>
+				</div>
+			</Form>
+		);
+	});
+	// add hander can change query with word from searchbar
+	const onChange = (q) => {
+		setQuery(q);
+	};
+	// get Ideas name, description, and images.
+	// const ideas = ['idea1', 'idea2', 'idea3', 'idea4', 'idea5', 'idea6', 'idea7'];
 
-  // get Ideas name, description, and images.
-  // const ideas = ['idea1', 'idea2', 'idea3', 'idea4', 'idea5', 'idea6', 'idea7'];
-  let ideas = response;
-  // const ideas = ['idea1', 'idea2', 'idea3', 'idea4', 'idea5', 'idea6', 'idea7'];
-  // Generate an array of box components
-  const generateBoxes = ideas.map((idea, idx) => {
-    return (
-      <Card key={idx} style={{ width: '18rem' }} className="m-2">
-        <Card.Img variant="top" src={idea.image} />
-        <Card.Body>
-          <Card.Title>{idea.name}</Card.Title>
-          <Card.Text>{idea.description}</Card.Text>
-          <NavLink
-            to={{
-              pathname: '/idea',
-              state: {
-                idea_id: idea.idea_id,
-              },
-            }}
-          >
-            <Button variant="primary"> Find out more </Button>
-          </NavLink>
-        </Card.Body>
-      </Card>
-    );
-  });
+	let ideas = response;
+	// filter Ideas array with query from state so if query is exist Ideas array will change
+	let sortedIdeas = ideas.filter((data) => {
+		return data.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+	});
 
-  // Search box component
-  const searchIdea = (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>
-          {' '}
-          <h1>May your dream come true</h1>{' '}
-        </Form.Label>
-        <Form.Control
-          size="lg"
-          type="text"
-          placeholder="Search your dream..."
-        />
-        {/* <Button variant="primary" type="submit" className='mt-2'></Button> */}
-      </Form.Group>
-    </Form>
-  );
+	// const ideas = ['idea1', 'idea2', 'idea3', 'idea4', 'idea5', 'idea6', 'idea7'];
+	// Generate an array of box components with sortedIdea
+	const generateBoxes = sortedIdeas.map((idea, idx) => {
+		return (
+			<Card key={idx} style={{ width: '18rem' }} className="m-2">
+				<Card.Img variant="top" src={idea.image} />
+				<Card.Body>
+					<Card.Title>{idea.name}</Card.Title>
+					<Card.Text>{idea.description}</Card.Text>
+					<NavLink
+						to={{
+							pathname: '/idea',
+							state: {
+								idea_id: idea.idea_id,
+							},
+						}}
+					>
+						<Button variant="primary"> Find out more </Button>
+					</NavLink>
+				</Card.Body>
+			</Card>
+		);
+	});
 
-  const explorePage = (
-    <Container fluid>
-      <Row>
-        <Col lg={3} className="mt-4">
-          <Row noGutters>
-            {' '}
-            <h2> Choose technology stack </h2>
-          </Row>
-          <div className="">{generateTech}</div>
-        </Col>
+	// Search box component
+	const searchIdea = (
+		<Form>
+			<Form.Group controlId="formBasicEmail">
+				<Form.Label>
+					{' '}
+					<h1>May your dream come true</h1>{' '}
+				</Form.Label>
+				<Form.Control
+					size="lg"
+					type="text"
+					placeholder="Search your dream..."
+					onChange={(e) => onChange(e.target.value)}
+				/>
+				{/* <Button variant="primary" type="submit" className='mt-2'></Button> */}
+			</Form.Group>
+		</Form>
+	);
 
-        <Col lg={9} class="d-flex align-items-center" className="mt-4">
-          {searchIdea}
-          <Row>{generateBoxes}</Row>
-        </Col>
-      </Row>
-    </Container>
-  );
+	const explorePage = (
+		<Container fluid>
+			<Row>
+				<Col lg={3} className="mt-4">
+					<Row noGutters>
+						{' '}
+						<h2> Choose technology stack </h2>
+					</Row>
+					<div className="">{generateTech}</div>
+				</Col>
 
-  return response.length === 1 ? (
-    <Spinner />
-  ) : (
-    <Fragment> {explorePage} </Fragment>
-  );
+				<Col lg={9} class="d-flex align-items-center" className="mt-4">
+					{searchIdea}
+					<Row>{generateBoxes}</Row>
+				</Col>
+			</Row>
+		</Container>
+	);
+
+	return response.length === 1 ? (
+		<Spinner />
+	) : (
+		<Fragment> {explorePage} </Fragment>
+	);
 };
 
 export default Explore;
